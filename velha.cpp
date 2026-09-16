74dd96d48e26c8bb95bd170624d8d137efa3c235
PedroCarpe <dd2426016@gmail.com>
2026-09-16 16:09:53 -0300

    refactor: substitui verificacoes manuais, por laco de repeticao nas linhas

--- velha.cpp
+++ velha.cpp
@@ -15,9 +15,19 @@
 
 int VerificaVelha( int velha[3][3] )
 {
-	if((velha[0][0]==1 && velha[0][1]==1 && velha[0][2]==1) or (velha[1][0]==1 && velha[1][1]==1 && velha[1][2]==1) or (velha[2][0]==1 && velha[2][1]==1 && velha[2][2]==1)){
-	    return 1;
-    }
+	
+	//Refatoração para a verificação generalizada das linhas
+	for (int i = 0; i < 3; i++) {
+        if (velha[i][0] == 1 && 
+            velha[i][0] == velha[i][1] && 
+            velha[i][1] == velha[i][2]) {
+            return velha[i][0]; // Retorna 1 (Jogador 1)
+            }
+        }
+	
+	//if((velha[0][0]==1 && velha[0][1]==1 && velha[0][2]==1) or (velha[1][0]==1 && velha[1][1]==1 && velha[1][2]==1) or (velha[2][0]==1 && velha[2][1]==1 && velha[2][2]==1)){
+	//    return 1;
+    //}
 	return 0; /*!< retorna zero para teste */ 
 }
 
