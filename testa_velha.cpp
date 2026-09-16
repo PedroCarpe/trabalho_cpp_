a86cdbe7c8715ff62bbfb68e688e3d30ef7001a2
PedroCarpe <dd2426016@gmail.com>
2026-09-16 15:47:06 -0300

    fix: adiciona novo trecho de codigo que, verifica vitoria do jogador 1 na 3ª linha

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -23,11 +23,11 @@ TEST_CASE( "Testa linhas, esperando vitória do jogador 1", "[vitoria 1]" ) {
 					  };
     REQUIRE( VerificaVelha(teste2) == 1 );
     
-	int teste2[3][3]= {   { 0, 2, 1 }, 
+	int teste3[3][3]= {   { 0, 2, 1 }, 
 	                      { 1, 2, 0 },
 						  { 1, 1, 1 }
 					  };
-    REQUIRE( VerificaVelha(teste2) == 1 );
+    REQUIRE( VerificaVelha(teste3) == 1 );
 
 
 
