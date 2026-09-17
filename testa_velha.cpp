cbdc40f6458ec0ed25b0a10d41933766f9b8576d
PedroCarpe <dd2426016@gmail.com>
2026-09-17 16:43:25 -0300

    fix: sucesso no caso de teste, para vitoria em colunas do jogador 2

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -81,18 +81,18 @@ TEST_CASE( "Testa colunas, esperando vitória do jogador 2", "[vitoria 2]" ) {
 	                      { 2, 1, 0 },
 						  { 2, 2, 1 }
 					  };
-    REQUIRE( VerificaVelha(teste1) == 0 );
+    REQUIRE( VerificaVelha(teste1) == 2 );
 
     int teste2[3][3]= {   { 1, 2, 1 }, 
 	                      { 1, 2, 1 },
 						  { 0, 2, 2 }
 					  };
-    REQUIRE( VerificaVelha(teste2) == 0 );
+    REQUIRE( VerificaVelha(teste2) == 2 );
     
 	int teste3[3][3]= {   { 0, 2, 2 }, 
 	                      { 1, 1, 2 },
 						  { 0, 1, 2 }
 					  };
-    REQUIRE( VerificaVelha(teste3) == 0 ); 
+    REQUIRE( VerificaVelha(teste3) == 2 ); 
 
 }	
