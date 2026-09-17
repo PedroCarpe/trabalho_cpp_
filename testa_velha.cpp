b51d6a52a1b3c87a84dc65cb7c7ccb459361100c
PedroCarpe <dd2426016@gmail.com>
2026-09-17 15:58:57 -0300

    test: adiciona caso de teste, para vitoria do jogador 1 em colunas

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -50,5 +50,28 @@ TEST_CASE( "Testa linhas, esperando vitória do jogador 2", "[vitoria 2]" ) {
 					  };
     REQUIRE( VerificaVelha(teste3) == 2 );
 
+}	
+
+
+
+TEST_CASE( "Testa colunas, esperando vitória do jogador 1", "[vitoria 1]" ) {
+	int teste1[3][3]= {   { 1, 0, 1 }, 
+	                      { 1, 0, 2 },
+						  { 1, 2, 1 }
+					  };
+    REQUIRE( VerificaVelha(teste1) == -1 );
+
+    int teste2[3][3]= {   { 2, 1, 1 }, 
+	                      { 2, 1, 1 },
+						  { 0, 1, 2 }
+					  };
+    REQUIRE( VerificaVelha(teste2) == -1 );
+    
+	int teste3[3][3]= {   { 0, 2, 1 }, 
+	                      { 1, 2, 1 },
+						  { 0, 1, 1 }
+					  };
+    REQUIRE( VerificaVelha(teste3) == -1 );
+
 } 
  
